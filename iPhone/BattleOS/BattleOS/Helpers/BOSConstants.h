1bae75e4601d82e5a4a6d7b8c2f379755941b95a
Max Tymchii <max@ekreative.com>
2013-02-23 18:56:32 +0200

    Add new constants.

--- iPhone/BattleOS/BattleOS/Helpers/BOSConstants.h
+++ iPhone/BattleOS/BattleOS/Helpers/BOSConstants.h
@@ -3,9 +3,26 @@
 
 #define APP_TITLE @"BattleOS"
 
+#define SWORD_IMAGE @"sword"
+#define SHIELD_IMAGE @"shield"
 
 #define USER_MONEY @"USER MONEY"
 #define USER_EXPERIENCE @"USER EXPERIENCE"
 #define USER_LEVEL @"USER LEVEL"
 #define USER_ATTACK_FORCE @"ATACK FORCE"
 #define USER_HEALTH @"USER HEALTH"
+
+//parser definitions
+#define OS_KEY @"os"
+#define IOS_KEY @"ios"
+#define ANDROID_KEY @"android"
+
+#define ENEMY_KEY @"enemy"
+#define HEALTH_KEY @"health"
+#define EXPERIENCE_KEY @"experience"
+#define LEVEL_KEY @"level"
+
+#define FIGHT_KEY @"fight"
+#define ATTACK_KEY @"attack"
+#define BLOCK_KEY @"block"
+#define POWER_KEY @"power"
