e01dad3d73bab842af5ee270501945946b8b6006
Max Tymchii <max@ekreative.com>
2013-02-23 11:42:29 +0200

    Implement initial bump code

--- iPhone/BattleOS/BattleOS/BOSViewController.m
+++ iPhone/BattleOS/BattleOS/BOSViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BOSViewController.h"
+#import "BumpClient.h"
 
 @interface BOSViewController ()
 
@@ -18,12 +19,57 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self configureBump];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) configureBump {
+    // userID is a string that you could use as the user's name, or an ID that is semantic within your environment
+    [BumpClient configureWithAPIKey:@"de703e6680454adbbf3d1ac99727c9b0" andUserID:[[UIDevice currentDevice] name]];
+    
+    [[BumpClient sharedClient] setMatchBlock:^(BumpChannelID channel) {
+        NSLog(@"Matched with user: %@", [[BumpClient sharedClient] userIDForChannel:channel]);
+        [[BumpClient sharedClient] confirmMatch:YES onChannel:channel];
+    }];
+    
+    [[BumpClient sharedClient] setChannelConfirmedBlock:^(BumpChannelID channel) {
+        NSLog(@"Channel with %@ confirmed.", [[BumpClient sharedClient] userIDForChannel:channel]);
+        [[BumpClient sharedClient] sendData:[[NSString stringWithFormat:@"Hello, world!"] dataUsingEncoding:NSUTF8StringEncoding]
+                                  toChannel:channel];
+    }];
+    
+    [[BumpClient sharedClient] setDataReceivedBlock:^(BumpChannelID channel, NSData *data) {
+        NSLog(@"Data received from %@: %@",
+              [[BumpClient sharedClient] userIDForChannel:channel],
+              [NSString stringWithCString:[data bytes] encoding:NSUTF8StringEncoding]);
+    }];
+    
+    
+    // optional callback
+    [[BumpClient sharedClient] setConnectionStateChangedBlock:^(BOOL connected) {
+        if (connected) {
+            NSLog(@"Bump connected...");
+        } else {
+            NSLog(@"Bump disconnected...");
+        }
+    }];
+    
+    // optional callback
+    [[BumpClient sharedClient] setBumpEventBlock:^(bump_event event) {
+        switch(event) {
+            case BUMP_EVENT_BUMP:
+                NSLog(@"Bump detected.");
+                break;
+            case BUMP_EVENT_NO_MATCH:
+                NSLog(@"No match.");
+                break;
+        }
+    }];
 }
 
 @end
